e4f516704947a8b8211998e402de2fb8b30240d4
Juan Karlo Licudine <arebel-mac@macrebel.local>
2018-01-31 22:33:18 +0800

    Testing out Cobalt.rs installation in Travis.

--- test/cobalt-test.el
+++ test/cobalt-test.el
@@ -5,6 +5,9 @@
 
 ;;; Code:
 
+(ert-deftest t-cobalt-version-test ()
+  (should (equal (shell-command-to-string "cobalt -V") "Cobalt 0.11.1\n") ))
+
 (ert-deftest t-convert-title-to-file-name ()
   (should (equal (cobalt--convert-title-to-file-name "This is a test") "this-is-a-test"))
   (should (equal (cobalt--convert-title-to-file-name "This is a test.") "this-is-a-test"))
