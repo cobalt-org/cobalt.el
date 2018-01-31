ec56bf077841d8437a732c93113884d9e74670d3
Juan Karlo Licudine <arebel-mac@macrebel.local>
2018-01-31 18:22:59 +0800

    Created functions related to cobalt serve.

--- cobalt.el
+++ cobalt.el
@@ -2,14 +2,34 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar cobalt-blog-paths '("~/blogs/accidentalrebel.github.com/" "~/blogs/karlolicudine-blog/"))
-(defvar cobalt-log-buffer-name "*cobalt*")
+(defvar cobalt-blog-paths '("~/blogs/accidentalrebel.github.com/" "~/blogs/karlolicudine-blog/")
+  "List of blogs of the user.")
+
+(defvar cobalt-log-buffer-name "*cobalt*"
+  "Name of the log buffer for cobalt process output.")
+
+(defvar cobalt--serve-process nil
+  "Use to save cobalt serve process is so it can be killed in the future.")
 
 (defun cobalt-version ()
   "Initialize cobalt."
   (interactive)
   (let ((default-directory (car cobalt-blog-paths)))
     (message "%s" (shell-command-to-string (concat (executable-find "cobalt") " -V")))))
+
+(defun cobalt-serve ()
+  "Build, serve, and watch the project at the source dir."
+  (interactive)
+  (let* ((default-directory (car cobalt-blog-paths)))
+    (setq cobalt--serve-process (start-process "cobalt-serve" cobalt-log-buffer-name (executable-find "cobalt") "serve"))
+    (when (not cobalt--serve-process)
+      (message "Error in running: cobalt serve"))))
+
+(defun cobalt-serve-kill ()
+  "Kill the cobalt serve process, if existing."
+  (interactive)
+  (when cobalt--serve-process
+    (kill-process cobalt--serve-process)))
 
 (defun cobalt-new-post (post-title)
   "Initialize cobalt with POST-TITLE."
