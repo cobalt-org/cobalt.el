7b0835884be754607fdb32f93753ee41ae991916
Juan Karlo Licudine <arebel-mac@macrebel.local>
2018-01-31 23:21:15 +0800

    Added a main test.

--- test/cobalt-test.el
+++ test/cobalt-test.el
@@ -4,9 +4,26 @@
 ;; These are the tests for `cobalt'
 
 ;;; Code:
+(require 'f)
 
 (ert-deftest t-cobalt-version-test ()
   (should (equal (shell-command-to-string "cobalt -V") "Cobalt 0.11.1\n") ))
+
+(ert-deftest t-cobalt-main ()
+  (within-sandbox
+   (setq-local test-blog-path (concat cobalt-sandbox-path "/test-blog/"))
+   (unless (f-exists? test-blog-path)
+     (f-mkdir test-blog-path))
+   
+   (setq-local cobalt-site-paths (list test-blog-path))
+   (setq-local cobalt--current-site (car cobalt-site-paths))
+   
+   (setq-local cobalt--serve-process nil)
+   (should-not cobalt--serve-process)
+   (cobalt-serve)
+   (should cobalt--serve-process)
+   (cobalt-serve-kill)
+   (should-not cobalt--serve-process)))
 
 (ert-deftest t-convert-title-to-file-name ()
   (should (equal (cobalt--convert-title-to-file-name "This is a test") "this-is-a-test"))
