2ace1cf742684874c4e3d8c5ca7fdfd5f6827cb3
Juan Karlo Licudine <accidentalrebel@gmail.com>
2018-02-10 11:02:10 +0800

    Added the package header details.

--- cobalt.el
+++ cobalt.el
@@ -1,9 +1,42 @@
 ;;; cobalt.el --- Summary   -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Juan Karlo Lidudine
+
+;; Author: Juan Karlo Licudine <accidentalrebel@gmail.com>
+;; URL: https://github.com/accidentalrebel/cobalt.el
+;; Version: 1.0.0
+;; Keywords: convenience
+;; Package-Requires: ((emacs "24"))
+
+;; This file is not part of GNU Emacs.
+
 ;;; Commentary:
-;; 
-;;; Code:
+
+;; Cobalt.el is an Emacs interface for Cobalt.rss, a static site
+;; generator written in Rust.
+
+;; The package provides simple-to-use Emacs commands for easy site
+;; generation and post management.
+
+;;; License:
+
+;; This program is free software; you can redistributfe it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Todo:
+
 ;; - Create function that checks if the current buffer is a valid post.
 ;; - Get the posts directory from the _cobalt.yml file.
 ;; - Only preview a buffer if it is a valid post.
@@ -11,6 +44,8 @@
 ;; - If start-process returns an error don't let it set cobalt--serve-process
 ;; - Fix error with cobalt-build when cobalt--current-site is nil.
 ;; - Add a license file.
+
+;;; Code:
 
 (defcustom cobalt-site-paths nil
   "Variable that holds a list of cobalt sites."
